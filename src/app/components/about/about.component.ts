bc4e744039ac5a343acbd0878368014644de84a0
Varshith Raj Basa <varshithraj.basa@gmail.com>
2021-10-25 09:13:11 +0530

    About component

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router,
+    private _snackBar:MatSnackBar) { }
 
   ngOnInit(): void {
   }
-
+  navigate(link){
+    this.router.navigateByUrl(link);
+  }
+  share(){
+    try{
+    navigator.share({
+      title: 'Crypto App(Encrypt & Decrypt)',
+      url: 'https://varshithrajbasa.github.io/CryptoApp'
+    }).then(() => {
+      this._snackBar.open('Thanks for Sharing...! 😊','Close',{
+        horizontalPosition: 'end',
+        verticalPosition: 'top',
+      });
+    })
+    .catch(()=>{
+      this._snackBar.open('Failed to Share 😢','Close',{
+        horizontalPosition: 'end',
+        verticalPosition: 'top',
+      });
+    });}
+    catch(err){
+      this._snackBar.open('Failed to Share 😢','Close',{
+        horizontalPosition: 'end',
+        verticalPosition: 'top',
+      });
+    }
+  }
 }
