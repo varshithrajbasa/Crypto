4b67c62715daccb33a23e67c1f613c51f7d05f6d
Varshith Raj Basa <varshithraj.basa@gmail.com>
2021-10-25 09:13:30 +0530

    Added 404 component

--- src/app/components/notfound/notfound.component.ts
+++ src/app/components/notfound/notfound.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-notfound',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotfoundComponent implements OnInit {
 
-  constructor() { }
+
+  t:number=5;
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.time()
+  }
+  time(){
+    setTimeout(() => {
+      this.t = this.t-1;
+      this.t == 0?this.router.navigateByUrl('/'):this.time()
+   }, 1000);
+
   }
 
 }
