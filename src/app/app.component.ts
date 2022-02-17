6ff4e1b9f5b6c8a023ba5aa93cf47d19e19765c1
Varshith Raj Basa <varshithraj.basa@gmail.com>
2022-02-17 10:25:02 +0530

    Fix issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,14 +23,17 @@ export class AppComponent implements OnInit,AfterViewInit{
   ngOnInit(){
   }
   ngAfterViewInit() {
-    this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
-      if (res.matches) {
-        this.sidenav.mode = 'over';
-        this.sidenav.close();
-      } else {
-        this.sidenav.mode = 'side';
-        this.sidenav.open();
-      }
-    });
+    setTimeout(() => {
+      
+      this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+        if (res.matches) {
+          this.sidenav.mode = 'over';
+          this.sidenav.close();
+        } else {
+          this.sidenav.mode = 'side';
+          this.sidenav.open();
+        }
+      });
+    }, 10);
   }
 }
